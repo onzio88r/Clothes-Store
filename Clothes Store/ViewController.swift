b443343b40e739961cc063e75d33e2dbf10437e0
Daniele Rapali <daniele.rapali@gmail.com>
2020-01-20 18:43:11 +0000

    Project creation

---
+++ Clothes Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clothes Store
+//
+//  Created by Daniele Rapali on 20/01/2020.
+//  Copyright © 2020 Daniele Rapali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
